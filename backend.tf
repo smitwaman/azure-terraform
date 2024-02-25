a2d491a3c4a06f05aa27ae26578f3b8345de4a25
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-25 12:40:12 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "az900"
+    resource_group_name  = "az-900"
     storage_account_name = "sparx900"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
